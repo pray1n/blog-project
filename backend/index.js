47161c4f4e5a132e138da8f409fae3675fec2839
Tilo Rabs <trabs@web.de>
2022-09-17 13:51:20 +0200

    model

--- backend/index.js
+++ backend/index.js
@@ -3,29 +3,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('build'));
 app.use(express.json());
+const blogs = require('./models/blogs');
 
 const port = process.env.PG_PORT || 8080;
-//title, date_time, author_name, content_text, picture, special, category_id
-const blogs = [
-    {
-        title:"SEYCHELLEN SEHENSWÜRDIGKEITEN – 12 WUNDERVOLLE DINGE, DIE DU GESEHEN UND GEMACHT HABEN SOLLTEST",
-        datetime:'2022-10-09',
-        author_name:'Maurice',
-        content_text:"Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.",
-        picture:"https://wetraveltheworld.de/wp-content/uploads/seychellen_sehenswurdigkeiten_beau-vallon-beach_01.jpg",
-        special: true,
-        category_id: 1
-    },
-    {
-        title:"FIJI – URLAUB VOM URLAUB 4",
-        datetime:'2022-10-09',
-        author_name:'Maurice',
-        content_text:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
-        picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
-        special: false,
-        category_id: 2
-    }
-]
 
 app.get('/blogs', (req, res) => {
     res.send(blogs);
---
+++ backend/models/blogs.js
@@ -0,0 +1,23 @@
+//title, date_time, author_name, content_text, picture, special, category_id
+const blogs = [
+    {
+        title:"SEYCHELLEN SEHENSWÜRDIGKEITEN – 12 WUNDERVOLLE DINGE, DIE DU GESEHEN UND GEMACHT HABEN SOLLTEST",
+        datetime:'2022-10-09',
+        author_name:'Maurice',
+        content_text:"Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.",
+        picture:"https://wetraveltheworld.de/wp-content/uploads/seychellen_sehenswurdigkeiten_beau-vallon-beach_01.jpg",
+        special: true,
+        category_id: 1
+    },
+    {
+        title:"CITY GUIDE AUGSBURG: 1 TAG IN DER EHEMALIGEN RÖMISCHEN PROVINZHAUPTSTADT [+HOTELS & RESTAURANTS]",
+        datetime:'2022-10-09',
+        author_name:'Moritz',
+        content_text:'Ein guter Startpunkt für unseren Stadtspaziergang durch Augsburg und zugleich eine sehr empfehlenswerte Unterkunft ist das Dom-Hotel.',
+        picture:'<img alt="seychellen" src="https://wetraveltheworld.de/wp-content/uploads/2021/11/augsburg-sehenswuerdigkeiten-an-einem-tag.jpg" width=150" height="70">',
+        special: false,
+        category_id: 2
+    }
+]
+
+module.exports = blogs;

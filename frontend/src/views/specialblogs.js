7cafe97b00fc802f45e959fad8a5a0eae9b57745
Tilo Rabs <trabs@web.de>
2022-09-17 17:44:29 +0200

    Update specialblogs.js

--- frontend/src/views/specialblogs.js
+++ frontend/src/views/specialblogs.js
@@ -1,10 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export default function SpecialBlogs({specialblogs}) {
     const li_specialblogs = specialblogs.map((blog) => {
+        const blog_to = "/" + blog.id;
         return (
             <section className="special" key={blog.id}>
                 <h4>{blog.title}</h4>
                 <p>{blog.content_text.substring(0, 40)} ...</p>
-                <a href="#">Read more</a>
+                <NavLink className="link" to={blog_to}>Read more</NavLink>
             </section>
         );
     });

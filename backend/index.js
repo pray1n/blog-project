ad9ae70396d3a462802dfcd13597f3d660160a45
pray1n <89249367+pray1n@users.noreply.github.com>
2022-09-17 11:41:27 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,52 @@ app.use(express.json())
 const port = 8080
 
 const blogs = [
-    {title:"FIJI – URLAUB VOM URLAUB",
+    {title:"FIJI – URLAUB VOM URLAUB 1",
     content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
     tag:"Strandurlaub",
     picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
     special: true,
 
     },
-    {title:"Blogpost 2",
-    date_time:"2022-10-09",
-    author_name:"Maurice",
-    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.Die Lagune ist aus dem gleichnamigen Film aus den 90ern bekannt. Sobald man ins Wasser geht wird man von den verschiedensten Fischschwärmen umzingelt, weil sie hier dauernd gefüttert werden. Die Tour ist ganz nett. Lange verweilen kann man an dem Strand nicht, weil es ein Privatstrand ist. Jedoch gehört der Strand nicht zum Blue Lagoon Resort, sondern zum Turtle Island Resort.`,
+    {title:"FIJI – URLAUB VOM URLAUB 2",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
     tag:"Strandurlaub",
     picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
-    special: true
+    special: false,
     },
-    {title:"Blogpost 3",
-    date_time:"2022-10-09",
-    author_name:"Maurice",
-    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.Die Lagune ist aus dem gleichnamigen Film aus den 90ern bekannt. Sobald man ins Wasser geht wird man von den verschiedensten Fischschwärmen umzingelt, weil sie hier dauernd gefüttert werden. Die Tour ist ganz nett. Lange verweilen kann man an dem Strand nicht, weil es ein Privatstrand ist. Jedoch gehört der Strand nicht zum Blue Lagoon Resort, sondern zum Turtle Island Resort.`,
+    {title:"FIJI – URLAUB VOM URLAUB 3",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
     tag:"Strandurlaub",
     picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
-    special: true
+    special: false,
     },
+    {title:"FIJI – URLAUB VOM URLAUB 4",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
+    tag:"Strandurlaub",
+    picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
+    special: false,
+    },
+    {title:"FIJI – URLAUB VOM URLAUB 5",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
+    tag:"Strandurlaub",
+    picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
+    special: true,
+
+    },
+    {title:"FIJI – URLAUB VOM URLAUB 6",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
+    tag:"Strandurlaub",
+    picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
+    special: false,
+
+    },
+    {title:"FIJI – URLAUB VOM URLAUB 7",
+    content_short:`Schnorchelausflug zur blauen Lagune Für 25,-$/Person kann man vom Resort aus mit dem Boot zum schnorcheln zur blauen Lagune fahren.`,
+    tag:"Strandurlaub",
+    picture:"https://wetraveltheworld.de/wp-content/uploads/2018/03/tallinn-sehenswuerdigkeiten_.jpg",
+    special: true,
+
+    }
 
 
 

b0b4c610145984a27cd0c34bd5c19977d4fcbf55
Tilo Rabs <trabs@web.de>
2022-09-24 10:45:07 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
-//const blogs = require('./models/blogs')
-//const categories = require('./models/categories')
-const db_operations = require('./controllers/db_operations')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('build'));
+app.use(cors());
+app.use(express.json());
+const blogs = require('./models/blogs'); //mock objects
+const categories = require('./models/categories'); //mock objects
+const { getBlogs, insertBlogPost } = require('./controllers/db_operations');
 
-const port = process.env.PG_PORT || 8080
+//const port = process.env.PG_PORT || 8080;
+const port = 8080;
 
 app.get('/blogs', (req, res) => {
-    const blogs = db_operations.getBlogs();
-    res.send(blogs)
+    //res.send(blogs); //for mock objects
+    getBlogs()
+    .then((data) => {res.json(data)})
+    .catch((err) => {
+        res.status(400).send({error: err.message})
+    })
 })
 
 app.get('/categories', (req, res) => {
-    //const categories = db_operations.getCategories();
-    //res.send(categories)
+    res.send(categories); //for mock objects
+    /* getCategories()
+    .then((data) => {res.json(data)})
+    .catch((err) => {
+        res.status(400).send({error: err.message})
+    }) */
 })
 
-app.listen(port, () => console.log('Server listening at ' + port))
+app.listen(port, () => console.log('Server listening at ' + port));
